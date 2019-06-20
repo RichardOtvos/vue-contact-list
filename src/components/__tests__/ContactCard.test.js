ed5b2771150e2f7fa3f22e0083418f81c14f8c87
Richard Otvos <richard.otvos@cloudera.com>
2019-06-20 13:18:50 +0200

    Adds unit tests for Contact Card component

--- src/components/__tests__/ContactCard.test.js
+++ src/components/__tests__/ContactCard.test.js
@@ -1,10 +1,63 @@
-import { mount } from "@vue/test-utils";
-import ContactCard from "../ContactCard";
+import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+import ContactCard from '../ContactCard';
 
-describe("ContactCard", () => {
-  test("is a Vue instance", () => {
+describe('ContactCard', () => {
+  test('is a Vue instance', () => {
     // expect(true).toBeTruthy();
-    const wrapper = mount(ContactCard);
+    const wrapper = shallowMount(ContactCard);
     expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test('should display the name correctly', () => {
+    const testProps = {
+      name: 'Joe Test',
+    };
+    const wrapper = shallowMount(ContactCard, {
+      propsData: testProps,
+    });
+
+    const compEl = wrapper.findAll('.contact-details p').at(0);
+
+    expect(compEl.text()).toContain(testProps.name);
+  });
+
+  test('should display the email correctly', () => {
+    const testProps = {
+      email: 'test@example.com',
+    };
+    const wrapper = shallowMount(ContactCard, {
+      propsData: testProps,
+    });
+
+    const compEl = wrapper.findAll('.contact-details a').at(0);
+
+    expect(compEl.text()).toContain(testProps.email);
+    expect(compEl.html()).toContain(`mailto:${testProps.email}`);
+  });
+
+  test('should display the bio correctly', () => {
+    const testProps = {
+      bio: 'This is a test',
+    };
+    const wrapper = shallowMount(ContactCard, {
+      propsData: testProps,
+    });
+
+    const compEl = wrapper.findAll('.contact-details p').at(2);
+
+    expect(compEl.text()).toContain(testProps.bio);
+  });
+
+  test('should display the avatar correctly', () => {
+    const testProps = {
+      name: 'Joe Test',
+      avatarUrl: 'http://mydomain.com/myavatar.png'
+    };
+    const wrapper = shallowMount(ContactCard, {propsData: testProps});
+
+    const compEl = wrapper.find('.contact-card img');
+
+    expect(compEl.html()).toContain(testProps.avatarUrl);
   });
 });
