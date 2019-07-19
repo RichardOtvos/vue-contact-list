27a13a3bf26fc54c27c65f9e1ff3125cf4c74ddc
Richard Otvos <richard.otvos@cloudera.com>
2019-07-19 17:43:00 +0200

    Adds debounced search to github search input

--- src/components/AddFromGithub.vue
+++ src/components/AddFromGithub.vue
@@ -8,15 +8,16 @@
         <md-autocomplete
           v-model="selectedGithubUser"
           md-highlight-text="true"
-          :md-options="users"
+          :md-options="githubSearchResults"
           :md-open-on-focus="false"
+          @md-changed="debouncedGhSearch"
         >
           <label>GitHub Username</label>
           <template slot="md-autocomplete-item" slot-scope="{ item, term }">
             <md-highlight-text :md-term="term">{{ item }}</md-highlight-text>
           </template>
           <template slot="md-autocomplete-empty">
-            No one was found with that name!
+            <br>
           </template>
         </md-autocomplete>
       </md-card-content>
@@ -25,13 +26,27 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'AddFromGithub',
   data() {
     return {
       selectedGithubUser: null,
-      users: ['Ricsi', 'Feri', 'Tomi', 'Norbi', 'Roli']
+      fakeUserList: ['Ricsi', 'Feri', 'Tomi', 'Norbi', 'Roli'],
+      githubSearchResults: [],
+      debouncedGhSearch: null
     };
+  },
+  methods: {
+    searchForGitHubUsers(searchTerm) {
+      this.githubSearchResults = new Promise((resolve, reject) => {
+        setTimeout(() => resolve([]), 1000);
+      });
+    }
+  },
+  created() {
+    this.debouncedGhSearch = _.debounce(this.searchForGitHubUsers, 1000);
   }
 };
 </script>
