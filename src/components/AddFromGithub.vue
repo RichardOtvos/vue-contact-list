615d7d15d6224fbf75628b6f713d1c2c3367881d
Richard Otvos <richard.otvos@cloudera.com>
2019-07-20 14:29:35 +0200

    Further develops add form github functionality with mock data

--- src/components/AddFromGithub.vue
+++ src/components/AddFromGithub.vue
@@ -6,21 +6,23 @@
       </md-card-header>
       <md-card-content>
         <md-autocomplete
-          v-model="selectedGithubUser"
+          :value="selectedGithubUserName"
           md-highlight-text="true"
           :md-options="githubSearchResults"
           :md-open-on-focus="false"
           @md-changed="debouncedGhSearch"
+          @md-selected="selectGhUser"
         >
           <label>GitHub Username</label>
-          <template slot="md-autocomplete-item" slot-scope="{ item, term }">
-            <md-highlight-text :md-term="term">{{ item }}</md-highlight-text>
+          <template slot="md-autocomplete-item" slot-scope="{ item, term }"
+            >{{ item.login }}
           </template>
           <template slot="md-autocomplete-empty">
-            <br>
+            <br />
           </template>
         </md-autocomplete>
       </md-card-content>
+      <md-button @click="addContact">send</md-button>
     </md-card>
   </form>
 </template>
@@ -28,12 +30,51 @@
 <script>
 import _ from 'lodash';
 
+let mockUsers = [
+  {
+    login: 'richardsondx',
+    id: 812376,
+    avatar_url: 'https://avatars0.githubusercontent.com/u/812376?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/richardsondx'
+  },
+  {
+    login: 'richarddurbin',
+    id: 3431398,
+    avatar_url: 'https://avatars3.githubusercontent.com/u/3431398?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/richarddurbin'
+  },
+  {
+    login: 'RichardKelley',
+    id: 833810,
+    avatar_url: 'https://avatars3.githubusercontent.com/u/833810?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/RichardKelley'
+  },
+  {
+    login: 'RichardBray',
+    id: 1377253,
+    avatar_url: 'https://avatars2.githubusercontent.com/u/1377253?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/RichardBray'
+  },
+  {
+    login: 'd6y',
+    id: 102661,
+    node_id: 'MDQ6VXNlcjEwMjY2MQ==',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/102661?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/d6y'
+  }
+];
+
 export default {
   name: 'AddFromGithub',
   data() {
     return {
       selectedGithubUser: null,
-      fakeUserList: ['Ricsi', 'Feri', 'Tomi', 'Norbi', 'Roli'],
+      selectedGithubUserName: '',
       githubSearchResults: [],
       debouncedGhSearch: null
     };
@@ -41,8 +82,15 @@ export default {
   methods: {
     searchForGitHubUsers(searchTerm) {
       this.githubSearchResults = new Promise((resolve, reject) => {
-        setTimeout(() => resolve([]), 1000);
+        setTimeout(() => resolve(mockUsers), 1000);
       });
+    },
+    selectGhUser(selectedUser) {
+      this.selectedGithubUser = selectedUser;
+      this.selectedGithubUserName = selectedUser.login;
+    },
+    addContact() {
+      console.log(this.selectedGithubUser);
     }
   },
   created() {
